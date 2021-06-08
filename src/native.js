e757a5c06a240f8b0c6f076157f63e8c77ff6d48
keshav raj <keshavkumark7@gmail.com>
2021-06-08 13:31:06 +0530

    Handled condition for webview reference

--- src/native.js
+++ src/native.js
@@ -1,21 +1,37 @@
-import { createMessager } from './messager/index'
+import { createMessager } from "./messager/index";
 
-export default getWebview => {
-    const { bind, define, listener: handler, fn, addEventListener, removeEventListener, isConnect } = createMessager(
-        (data) => getWebview().postMessage(JSON.stringify(data))
-    )
-    return {
-        bind, define, fn,
-        listener: evt => {
-            let data
-            try {
-                // FIX: webpack hotloader will triger this
-                data = JSON.parse(evt.nativeEvent.data)
-            } catch (e) {
-                __DEV__ && console.warn(e.message, evt.nativeEvent.data)
-            }
-            data && handler(data)
-        },
-        addEventListener, removeEventListener, isConnect
+export default (getWebview) => {
+  const {
+    bind,
+    define,
+    listener: handler,
+    fn,
+    addEventListener,
+    removeEventListener,
+    isConnect,
+  } = createMessager((data) => {
+    /*Call postMessage only if WebView Reference is valid.*/
+    const webviewReference = getWebview();
+    if (webviewReference) {
+      webviewReference.postMessage(JSON.stringify(data));
     }
-}
+  });
+  return {
+    bind,
+    define,
+    fn,
+    listener: (evt) => {
+      let data;
+      try {
+        // FIX: webpack hotloader will triger this
+        data = JSON.parse(evt.nativeEvent.data);
+      } catch (e) {
+        __DEV__ && console.warn(e.message, evt.nativeEvent.data);
+      }
+      data && handler(data);
+    },
+    addEventListener,
+    removeEventListener,
+    isConnect,
+  };
+};
